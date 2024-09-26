8490fe55d0cd55efdc4202290fdec7b6bb63c206
drexel-conexus <drexel.timogtimog@conexus-tech.com>
2024-09-26 20:11:28 +0800

    feat: Enable CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('')
     .setDescription(
